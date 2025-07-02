767e6a8a66401121481c2811722bc7dc8ee8bd6f
finharris <mybutiskfc@gmail.com>
2025-07-02 16:07:18 +0100

    todos

--- contentScripts/buttons.js
+++ contentScripts/buttons.js
@@ -6,6 +6,7 @@ const findLastWatchedButton = document.createElement("div");
 //   alt="find last watched button"
 //   class="button-icon"
 // />
+// TODO - make icon work
 findLastWatchedButton.innerHTML = `
 <p>YT PU Actions</p>
 `;
@@ -14,5 +15,5 @@ findLastWatchedButton.classList.add("button");
 buttonsContainer.insertAdjacentElement("beforebegin", findLastWatchedButton);
 
 findLastWatchedButton.addEventListener("click", () => {
-  console.log(true);
+  console.log(true); // TODO - make dropdown actions list of features
 });
